53d1dc1c60f075c237b14527aa6e8b956491435d
Paul Barreto <six.stream@gmail.com>
2023-01-15 21:33:15 -0400

    Add files via upload

---
+++ agent.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 11 21:36:22 2023
+
+@author: Pol git code by Jesus P 
+"""
+from contextlib import nullcontext
+import numpy as np
+
+class TwoArmedBandit():
+    def __init__(self, alpha=1, epsilon =0.1):
+        self.arms = 2
+        self.alpha = alpha
+        self.epsilon = epsilon
+        self.reset()
+
+    def reset(self):
+        self.action = 0
+        self.reward = 0
+        self.iteration = 0
+        self.values = np.zeros(self.arms)
+
+    def update(self, action, reward):
+        self.action = action
+        self.reward = reward
+        self.iteration += 1
+        self.values[action] = self.values[action] + self.alpha * (reward - self.values[action])
+
+    def get_action(self, mode):
+        if mode == 'random':
+            return np.random.choice(self.arms)
+        elif mode == 'greedy':
+            return np.argmax(self.values)
+        elif mode == 'epsilon-greedy':  
+            if np.random.rand() < self.epsilon:
+                return np.random.randint(self.arms)
+            else:
+                return np.argmax(self.values)
+            
+
+
+
+    def render(self):
+        print("Iteration: {}, Action: {}, Reward: {}, Values: {}".format(
+            self.iteration, self.action, self.reward, self.values))
---
+++ main.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 11 21:38:24 2023
+
+@author: hip_d git code by Jesus P 
+"""
+import gym
+import gym_environments
+from agent import TwoArmedBandit
+
+env = gym.make('TwoArmedBandit-v0')
+agent = TwoArmedBandit(0.1) 
+
+env.reset()
+
+for iteration in range(100):
+    action = agent.get_action("epsilon-greedy")    
+    _, reward, _, _, _ = env.step(action)
+    agent.update(action, reward) 
+    agent.render()
+
+env.close()
